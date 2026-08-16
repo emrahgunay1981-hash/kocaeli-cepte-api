d95012a9364a72a6377aa5e948446672289a3a8a
emrahgunay1981-hash <emrahgunay1981@gmail.com>
2026-08-16 07:32:51 +0300

    Kocaelispor API güncellemesi

--- api/kocaelispor.js
+++ api/kocaelispor.js
@@ -14,10 +14,10 @@ export default async function handler(req, res) {
     }
 
     const url =
-      `${API_URL}?team=1957&season=2026&next=10&timezone=Europe/Istanbul`;
+      `${API_URL}?team=1957&next=10&timezone=Europe/Istanbul`;
 
     const response = await fetch(url, {
-      headers: {
+  6    headers: {
         "x-apisports-key": apiKey,
         "Accept": "application/json"
       }
