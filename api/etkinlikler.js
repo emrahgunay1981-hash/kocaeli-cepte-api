3b0c61075cf301c1b194488d487c51dec63bc8b9
emrahgunay1981-hash <emrahgunay1981@gmail.com>
2026-08-30 10:26:02 +0300

    Update etkinlikler.js

--- api/etkinlikler.js
+++ api/etkinlikler.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  export default async function handler(req, res) {
   // CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
@@ -9,7 +8,6 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  try {
   try {
     const response = await fetch(
       "https://www.kocaeliseyret.com/kocaeli-etkinlikler",
